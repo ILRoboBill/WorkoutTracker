79ef22f1a4772d59bb413c247bbb85c5699e34ea
ILRoboBill <peterson.bille@yahoo.com>
2017-12-30 23:56:32 -0600

    Added the ability to change the values based off of the choices done in the Spinner

--- feature/src/main/java/com/example/dad/workouttracker/feature/MainActivity.java
+++ feature/src/main/java/com/example/dad/workouttracker/feature/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.dad.workouttracker.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.NumberPicker;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     NumberPicker npSetRep1 = null;
     NumberPicker npSetRep2 = null;
     NumberPicker npSetWeight1 = null;
@@ -14,14 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity:OnCreate","Did this actually work?");
 
-        // TODO: Add calling the setup up for weight changer
+        // TODO: add the ability to pull Excercises from stored memory
+        // TODO: add the ability to store Excercises to memory
+        // TODO: remove hard coding of excercises
+
        // WeightChanger(50, 100, 5,70,R.id.npSet1Weight);
         // Initialize the components
-        npSetRep1 =(NumberPicker)findViewById(R.id.npSet1Reps);
-        npSetRep2 =(NumberPicker)findViewById(R.id.npSet2Reps);
-        npSetWeight1 = (NumberPicker)findViewById(R.id.npSet1Weight);
-        npSetWeight2 = (NumberPicker)findViewById(R.id.npSet2Weight);
+        npSetRep1 =findViewById(R.id.npSet1Reps);
+        npSetRep2 =findViewById(R.id.npSet2Reps);
+        npSetWeight1 = findViewById(R.id.npSet1Weight);
+        npSetWeight2 = findViewById(R.id.npSet2Weight);
+
 
         // Initialize the Number Pickes
         SetupRepitions(15, npSetRep1);
@@ -29,11 +38,19 @@ public class MainActivity extends AppCompatActivity {
         WeightChanger(50, 100,5,75, npSetWeight1);
         WeightChanger(50, 100,5,95, npSetWeight2);
 
-
+        addListenerOnSpinnerItemSelection();
 
     }
 
-
+    /**
+     * This function creates the different Weight values for an excercise.  It creates
+     * strings for all of the values and then selects the startvalue
+     * @param minValue - Lowest weight
+     * @param maxValue - Maximum weight on the equipment
+     * @param step - Step for each weight
+     * @param startValue - Value that the Number Picker should start at
+     * @param np - Number Picker that the values are for
+     */
     protected void  WeightChanger(int minValue, int maxValue, int step, int startValue, NumberPicker np) {
 
         int idxStartValue = 0;
@@ -59,8 +76,40 @@ public class MainActivity extends AppCompatActivity {
 
         npSet.setMinValue(1);
         npSet.setMaxValue(25);
-        npSet.setValue(15);
+        npSet.setValue(startvalue);
         npSet.setWrapSelectorWheel(false);
+
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        Spinner spinner1 = (Spinner) findViewById(R.id.spnrExcercises);
+        spinner1.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+        String SelectedWeight = parent.getItemAtPosition(pos).toString();
+        Log.d("WeightChangeListener:onItemSelected","On Item Selected:" + SelectedWeight);
+        Log.d("WeightChangeListener::OnItemSelected","Id:" + R.id.npSet1Weight);
+
+        npSetWeight1 = (NumberPicker)findViewById(R.id.npSet1Weight);
+        npSetWeight2 = (NumberPicker)findViewById(R.id.npSet2Weight);
+
+        if(SelectedWeight.equalsIgnoreCase("Chest Press")){
+            WeightChanger(50,200,5,190,npSetWeight1);
+            WeightChanger( 50,200,5,50,npSetWeight2);
+            Log.d("**** WeightChangeListener:OnSelected", "Changed Weight Values");
+        }else if(SelectedWeight == "Row") {
+            // TODO: Set the appropriate Weights and Repititions for Chest Press
+        }else if (SelectedWeight == "Twister") {
+            // TODO: Set the appropriate Weights and Repititions for Twister
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
